2a2965ef76e3f568599641f149b22226a52909a2
SantiUWU <santiagoalfonsogarrido@gmail.com>
2023-09-29 19:21:25 -0300

    Menú de Pausa

---
+++ Juego_Parcial/Assets/Scripts/Menu_Pausa.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Pausa : MonoBehaviour
+{
+    [SerializeField] private GameObject botonPausa;
+    [SerializeField] private GameObject MenuPausa;
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        botonPausa.SetActive(false) ;
+        MenuPausa.SetActive(true) ;
+    }
+
+    public void Reanudar()
+    {
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        MenuPausa.SetActive(false);
+    }
+
+    public void menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
